669a4d5d344df5a331e8e270d4c142ff3aa2eac4
mtle <mtle@users.noreply.github.com>
2014-12-03 17:58:51 -0500

    Create BestTimetoBuyandSellStock.cpp

---
+++ BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,10 @@
+/***
+    Say you have an array for which the ith element is the 
+    price of a given stock on day i.
+
+    If you were only permitted to buy one share of the stock and 
+    sell one share of the stock, design an algorithm to find the 
+    best times to buy and sell.
+    The question is equivalent to the following:
+      Find i and j that maximizes Aj – Ai, where i < j.
+*/
