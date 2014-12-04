5539ece38f1eab8f21bf333af93f84e27013adc6
mtle <mtle@users.noreply.github.com>
2014-12-03 19:29:10 -0500

    Update kthSmallestElementIntTheUnionofTwoSortedArrays.cpp

--- kthSmallestElementIntTheUnionofTwoSortedArrays.cpp
+++ kthSmallestElementIntTheUnionofTwoSortedArrays.cpp
@@ -20,3 +20,84 @@ correction) of the two is incremented one step. The k-th smallest
 is obtained by traversing a total of k steps. This algorithm is very 
 similar to finding intersection of two sorted arrays.
 */
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <memory>
+#include <cstdlib>
+#include <vector>
+#include <new>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <stdexcept>
+#include <cassert>
+
+typedef unsigned int uint;
+using namespace std;
+
+template<typename T> void printArray(const vector<T> &v, int lo=0, int hi=0) const;
+template<typename T> void printVector(const vector<T> &v, int lo=0, int hi=0) const;
+template<typename T> void printMatrix(const vector<vector<T>>&, int row=0, int col=0) const;
+
+///////////////////////////////////////////////////////////////////////
+
+template<typename T> T Max(T x, T y)
+{
+	return (x >= y) ? x : y;
+}
+
+template<typename T> void printMatrix(const vector<vector<T>>& v, int row, int col) const
+{
+	for( auto j=0; j<col; ++j ) {
+   	 cout<<endl;
+   	 for ( auto i=0; i<row; ++i ) {
+   		 cout<<v[j][i]<<" ";
+   	 }
+	}
+}
+
+template<typename T> void printArray(const vector<T> &v, int lo, int hi) const
+{
+	cout<<"\n[";
+    if ( 0==lo && 0==hi ) {
+   	 for ( auto x : v ) {
+   		 cout<<x<<", ";
+   	 }
+    } else if ( lo>=0 && hi>0 ) {
+   	 for (auto x=lo; x<=hi; ++x) {
+   		 cout<<v.at(x) << ", ";
+   	 }
+    } else {
+    	return;
+	}
+	cout<<"]"<<endl;
+}
+
+template<typename T> void printVector(const vector<T> &v, int lo, int hi) const
+{
+	cout<<"\n[";
+    if ( 0==lo && 0==hi ) {
+   	 for ( auto x : v ) {
+   		 cout<<x<<", ";
+   	 }
+    } else if ( lo>=0 && hi>0 ) {
+   	 for (auto x=lo; x<=hi; ++x) {
+   		 cout<<v.at(x) << ", ";
+   	 }
+    } else {
+    	return;
+	}
+	cout<<"]"<<endl;
+}
+
+/*****************************************************************************/
+int main()
+{
+  
+}
