fa518a14b7896de744d487ecb4a19ae62920e4ed
mtle <mtle@users.noreply.github.com>
2014-12-04 14:16:38 -0500

    Update BestTimetoBuyandSellStock.cpp

--- BestTimetoBuyandSellStock.cpp
+++ BestTimetoBuyandSellStock.cpp
@@ -29,27 +29,36 @@
 typedef unsigned int uint;
 using namespace std;
 
-#define BITCLR32(x,y) ((~(1U<<y)|(1U<<(y-1)))&x)
-#define SWAPBITS(x,h,l) \
-if ( ((x>>l)&1) ^ ((x>>h)&1) ) x ^= ((1U<<l)|(1U<<h)); \
-else (void)0	⇐ intentionally leave out “;”
-#define swapbits(x,h,l) \
-do { \
-if ( ((x>>l)&1) ^ ((x>>h)&1) ) x ^= ((1U<<l)|(1U<<h)); \
-} while(false)  ⇐ intentionally leave out “;”
-    
-template<typename T> void printArray(const vector<T> &v, int lo=0, int hi=0) const;
-template<typename T> void printVector(const vector<T> &v, int lo=0, int hi=0) const;
-template<typename T> void printMatrix(const vector<vector<T>>&, int row=0, int col=0) const;
+template<typename T> void printArray(const vector<T> &v, int lo=0, int hi=0);
+template<typename T> void printVector(const vector<T> &v, int lo=0, int hi=0);
+template<typename T> void printMatrix(const vector<vector<T>>&, int row=0, int col=0);
 
 ///////////////////////////////////////////////////////////////////////
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec) {
+        os << "[" << el << "] ";
+    }
+    return os;
+}
+typedef vector<vector<int>> Matrix;
+void print(const Matrix& m)
+{
+    for (auto it=m.begin(); it!=m.end(); ++it) {
+		cout<<endl;
+		for (auto itr=it->begin(); itr!=it->end();++itr)
+			cout<<*itr <<" ";
+		}
+	cout<<endl;
+}
 
 template<typename T> T Max(T x, T y)
 {
 	return (x >= y) ? x : y;
 }
 
-template<typename T> void printMatrix(const vector<vector<T>>& v, int row, int col) const
+template<typename T> void printMatrix(const vector<vector<T>>& v, int row, int col)
 {
 	for( auto j=0; j<col; ++j ) {
    	 cout<<endl;
@@ -59,7 +68,7 @@ template<typename T> void printMatrix(const vector<vector<T>>& v, int row, int c
 	}
 }
 
-template<typename T> void printArray(const vector<T> &v, int lo, int hi) const
+template<typename T> void printArray(const vector<T> &v, int lo, int hi)
 {
 	cout<<"\n[";
     if ( 0==lo && 0==hi ) {
@@ -76,7 +85,7 @@ template<typename T> void printArray(const vector<T> &v, int lo, int hi) const
 	cout<<"]"<<endl;
 }
 
-template<typename T> void printVector(const vector<T> &v, int lo, int hi) const
+template<typename T> void printVector(const vector<T> &v, int lo, int hi)
 {
 	cout<<"\n[";
     if ( 0==lo && 0==hi ) {
@@ -94,8 +103,27 @@ template<typename T> void printVector(const vector<T> &v, int lo, int hi) const
 }
 
 /***********************************************************************************/
+int buySell(vector<int>& v)
+{
+	int max_profit=0;
+	int cur_profit;
+	int buy=v[0];
+	
+	// v = 12, 7, 8, 15, 16, 11, 9, 10
+	for (size_t x=1; x<v.size(); ++x) {
+		cur_profit = v[x] - buy;
+		if (cur_profit >= max_profit) {
+			max_profit = cur_profit;
+		} else {
+			buy = v[x];
+			max_profit = 0;
+		}
+	}
+	return max_profit;
+}
+
 int main()
 {
     
 }
-}
+
