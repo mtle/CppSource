4adcada6257d4005e73ac6bff206ee330157e53d
mtle <mtle@users.noreply.github.com>
2014-12-03 19:28:46 -0500

    Update RectanglesOverlap.cpp

--- RectanglesOverlap.cpp
+++ RectanglesOverlap.cpp
@@ -7,3 +7,84 @@ If you are coming up with a complicated set of conditionals, you might think
 too hard. There is an easier way. Try to think in the opposite direction.
 
 */
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <memory>
+#include <cstdlib>
+#include <vector>
+#include <new>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <stdexcept>
+#include <cassert>
+
+typedef unsigned int uint;
+using namespace std;
+
+template<typename T> void printArray(const vector<T> &v, int lo=0, int hi=0) const;
+template<typename T> void printVector(const vector<T> &v, int lo=0, int hi=0) const;
+template<typename T> void printMatrix(const vector<vector<T>>&, int row=0, int col=0) const;
+
+///////////////////////////////////////////////////////////////////////
+
+template<typename T> T Max(T x, T y)
+{
+	return (x >= y) ? x : y;
+}
+
+template<typename T> void printMatrix(const vector<vector<T>>& v, int row, int col) const
+{
+	for( auto j=0; j<col; ++j ) {
+   	 cout<<endl;
+   	 for ( auto i=0; i<row; ++i ) {
+   		 cout<<v[j][i]<<" ";
+   	 }
+	}
+}
+
+template<typename T> void printArray(const vector<T> &v, int lo, int hi) const
+{
+	cout<<"\n[";
+    if ( 0==lo && 0==hi ) {
+   	 for ( auto x : v ) {
+   		 cout<<x<<", ";
+   	 }
+    } else if ( lo>=0 && hi>0 ) {
+   	 for (auto x=lo; x<=hi; ++x) {
+   		 cout<<v.at(x) << ", ";
+   	 }
+    } else {
+    	return;
+	}
+	cout<<"]"<<endl;
+}
+
+template<typename T> void printVector(const vector<T> &v, int lo, int hi) const
+{
+	cout<<"\n[";
+    if ( 0==lo && 0==hi ) {
+   	 for ( auto x : v ) {
+   		 cout<<x<<", ";
+   	 }
+    } else if ( lo>=0 && hi>0 ) {
+   	 for (auto x=lo; x<=hi; ++x) {
+   		 cout<<v.at(x) << ", ";
+   	 }
+    } else {
+    	return;
+	}
+	cout<<"]"<<endl;
+}
+
+/*****************************************************************************/
+int main()
+{
+  
+}
